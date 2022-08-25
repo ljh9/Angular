9d90211eaa2cef3d8cdbdf146e6f3b9c41f279f0
ljh9 <dkssud2208@naver.com>
2022-08-25 21:28:41 +0900

    Adding Ingredients to Recipe

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
-        new Recipe('A Test Recipe22', 'This is simply a test22', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+        new Recipe(
+            'A Test Recipe', 
+            'This is simply a test', 
+            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+            [
+                new Ingredient('Meat', 1), 
+                new Ingredient('French Fires', 20)
+            ]),
+        new Recipe(
+            'A Test Recipe22', 
+            'This is simply a test22', 
+            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+            [
+                new Ingredient('Buns', 2), 
+                new Ingredient('Meat', 1)
+            ])
         ];
 
     getRecipes() {
