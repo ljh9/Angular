c8e6d602d4dd4696951c26afc3300f604a84c60d
ljh9 <dkssud2208@naver.com>
2022-09-10 15:03:13 +0900

    Adding delete and clear (cancel) functions

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Recipe } from '../recipe.model';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -15,7 +14,8 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute, 
-    private recipeService: RecipeService) { }
+    private recipeService: RecipeService, 
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -38,6 +38,7 @@ export class RecipeEditComponent implements OnInit {
     }else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onAddIngredient() {
@@ -50,6 +51,10 @@ export class RecipeEditComponent implements OnInit {
         ])
       })
     );
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
   private initForm(){
