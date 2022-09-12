86694863c7591dc549be3aab22b9b775a530e5e8
ljh9 <dkssud2208@naver.com>
2022-09-12 14:28:48 +0900

    Sending a POST request

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-start';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http.post('https://http-start-21c7e-default-rtdb.firebaseio.com/posts.json', postData).subscribe(
+      respnseData => {
+        console.log(respnseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
 }
