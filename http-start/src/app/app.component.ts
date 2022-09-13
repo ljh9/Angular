e030a717c3e1d89f58dc7e95c2e167bace0f1eb4
ljh9 <dkssud2208@naver.com>
2022-09-13 20:01:39 +0900

    Show loading indicator

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
     .get<{ [key: string]: Post }>('https://http-start-21c7e-default-rtdb.firebaseio.com/posts.json')
     .pipe(
@@ -50,6 +52,7 @@ export class AppComponent implements OnInit {
       return postsArray;
     }))
     .subscribe(posts => {
+      this.isFetching = false;
       this.loadedPosts = posts;
     });
   }
