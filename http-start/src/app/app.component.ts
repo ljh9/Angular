c678cd5ede2e49cf5b67aeb3e6deecdaebf5656f
ljh9 <dkssud2208@naver.com>
2022-09-13 21:13:51 +0900

    Handling Errors

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -12,15 +12,20 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
+    this.postsService.fetchPosts().subscribe(
+      posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
-    });
+      }, error => {
+        this.error = error.message;
+      }
+    );
   }
 
   onCreatePost(postData:  Post ) {
@@ -34,6 +39,8 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
     });
   }
 
