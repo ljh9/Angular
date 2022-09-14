147c05d463f9ce156dc34d5c2fc7f96b6fb5181e
ljh9 <dkssud2208@naver.com>
2022-09-14 19:54:09 +0900

    Setting the header

--- http-start/src/app/posts.service.ts
+++ http-start/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Post } from './post.model';
 import { catchError, map } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -27,7 +27,11 @@ export class PostsService{
 
     fetchPosts(){
         return this.http
-            .get<{ [key: string]: Post }>('https://http-start-21c7e-default-rtdb.firebaseio.com/posts.json')
+            .get<{ [key: string]: Post }>('https://http-start-21c7e-default-rtdb.firebaseio.com/posts.json', 
+              {
+                headers: new HttpHeaders({"Custom-Header": 'Hello'})
+              }
+            )
             .pipe(
                 map(responseData => {
                     const postsArray: Post[] = [];
