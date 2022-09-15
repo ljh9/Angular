baaa6f4999841b8ab3396b6773142f1790b140ed
ljh9 <dkssud2208@naver.com>
2022-09-15 14:16:31 +0900

    Saving recipes via effects

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipesService: RecipeService) {}
+
+    storeRecipes() {
+        const recipe = this.recipesService.getRecipes();
+        this.http
+        .put(
+            'https://ng-course-recipe-book-9edc3-default-rtdb.firebaseio.com/recipes.json', 
+            recipe
+        )
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
+
 }
