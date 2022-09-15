2afcf189fa4d41b5b7661c68d0be47925e1885b1
ljh9 <dkssud2208@naver.com>
2022-09-15 14:47:50 +0900

    Transforming Response Data

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -8,24 +8,25 @@ import { Recipe } from "./recipe.model";
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe', 
-            'This is simply a test', 
-            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
-            [
-                new Ingredient('Meat', 1), 
-                new Ingredient('French Fires', 20)
-            ]),
-        new Recipe(
-            'A Test Recipe22', 
-            'This is simply a test22', 
-            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
-            [
-                new Ingredient('Buns', 2), 
-                new Ingredient('Meat', 1)
-            ])
-        ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe', 
+    //         'This is simply a test', 
+    //         'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+    //         [
+    //             new Ingredient('Meat', 1), 
+    //             new Ingredient('French Fires', 20)
+    //         ]),
+    //     new Recipe(
+    //         'A Test Recipe22', 
+    //         'This is simply a test22', 
+    //         'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+    //         [
+    //             new Ingredient('Buns', 2), 
+    //             new Ingredient('Meat', 1)
+    //         ])
+    //     ];
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService){}
 
