0beb851a326c052d698ddba32b3e0d55d99250e2
ljh9 <dkssud2208@naver.com>
2022-09-16 20:05:34 +0900

    Sending a subscription request

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService{
 
     singup(email: string, password: string){
         return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyClR4ER4yvjOS40QXzLvjieiByB-MLRFDs', 
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyClR4ER4yvjOS40QXzLvjieiByB-MLRFDs', 
             {
                 email: email, 
                 password: password, 
