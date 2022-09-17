9787a2e53029fe56ad47c19041399c578a51b96a
ljh9 <dkssud2208@naver.com>
2022-09-17 22:52:56 +0900

    Adding Auto Login with NgRx

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -47,6 +47,25 @@ export class AuthService{
         }));
     }
 
+    autoLogin(){
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData')) ;
+        if(!userData){
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+        }
+
+    }
+
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -54,8 +73,9 @@ export class AuthService{
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-            const user = new User(email, userId, token, expirationDate);
-            this.user.next(user);
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse){
