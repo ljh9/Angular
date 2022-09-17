a79e20693d1063a3d776ea7ddd004a33f7dfce83
ljh9 <dkssud2208@naver.com>
2022-09-17 22:31:30 +0900

    Attaching tokens with interceptors

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -22,26 +22,20 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1), 
-            exhaustMap(user => {
-                return this.http.get<Recipe[]>(
-                    'https://ng-course-recipe-book-9edc3-default-rtdb.firebaseio.com/recipes.json', 
-                    {
-                        params: new HttpParams().set('auth', user.token)
-                    }
-                );
-            }), 
-            map(recipes => {
-                return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
-                    };
-                });
-            }), 
-            tap(recipes => {
-                this.recipesService.setRecipes(recipes);
-            })
-        );
+        return this.http
+            .get<Recipe[]>(
+                'https://ng-course-recipe-book-9edc3-default-rtdb.firebaseio.com/recipes.json'
+            ).pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+                        };
+                    });
+                }), 
+                tap(recipes => {
+                    this.recipesService.setRecipes(recipes);
+                })
+            );
     }
 
 }
