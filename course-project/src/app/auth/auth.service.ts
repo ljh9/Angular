74ab58d72e93348a9cf5c07ee0227c4d772635ac
ljh9 <dkssud2208@naver.com>
2022-09-17 20:26:15 +0900

    Creating and storing user data

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { catchError, Subject, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
     kind: string;
@@ -15,6 +16,8 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService{
+    user =new Subject<User>();
+
     constructor(private http: HttpClient) {}
 
     singup(email: string, password: string){
@@ -25,7 +28,9 @@ export class AuthService{
                 password: password, 
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError), tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        }));
     }
 
     login(email: string, password: string){
@@ -37,6 +42,12 @@ export class AuthService{
                 returnSecureToken: true
             }
         ).pipe(catchError(this.handleError));
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+            const user = new User(email, userId, token, expirationDate);
+            this.user.next(user);
     }
 
     private handleError(errorRes: HttpErrorResponse){
