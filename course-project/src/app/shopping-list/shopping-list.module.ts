294368b68fbca14213a43b83f33055d9aba7f1e8
ljh9 <dkssud2208@naver.com>
2022-09-22 18:19:08 +0900

    Loading Services Differently

--- course-project/src/app/shopping-list/shopping-list.module.ts
+++ course-project/src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { LoggingService } from "../logging.service";
 import { SharedModule } from "../shared/shared.module";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
@@ -15,8 +16,9 @@ import { ShoppingListComponent } from "./shopping-list.component";
         RouterModule.forChild([
             { path: '', component: ShoppingListComponent },
         ]), 
-        SharedModule
-    ]
+        // SharedModule
+    ], 
+    providers: [LoggingService]
 })
 export class ShoppingListModule{
 
