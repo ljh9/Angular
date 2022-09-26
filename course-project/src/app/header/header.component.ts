7965a11d4612b78fa7d72f208853c005a4519bc7
ljh9 <dkssud2208@naver.com>
2022-09-26 20:24:51 +0900

    End of authentication

--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Store } from "@ngrx/store";
+import { map, Subscription } from "rxjs";
 import { AuthService } from "../auth/auth.service";
 import { DataStorageService } from "../shared/data-storage.service";
+import * as fromApp from '../store/app.reducer';
 
 @Component({
     selector: 'app-header',
@@ -11,12 +13,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
     isAuthenticated = false;
     private userSub: Subscription;
 
-    constructor(private dataStorageService: DataStorageService, private authService: AuthService){}
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService, 
+        private store: Store<fromApp.AppState>){}
 
     collapsed = true;
 
     ngOnInit(){
-        this.userSub = this.authService.user.subscribe(user => {
+        // this.userSub = this.authService.user.subscribe(user => {
+        this.userSub = this.store.select('auth').pipe(map(authState => authState.user))
+        .subscribe(user => {
             // this.isAuthenticated = !user ? false: true;
             this.isAuthenticated = !!user;
             console.log(!user);
