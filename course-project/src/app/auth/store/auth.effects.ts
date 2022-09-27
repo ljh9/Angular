b857de7bbef1619910a3d83010325b5e3542a993
ljh9 <dkssud2208@naver.com>
2022-09-28 00:21:55 +0900

    End Login Effect

--- course-project/src/app/auth/store/auth.effects.ts
+++ course-project/src/app/auth/store/auth.effects.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Actions, ofType, Effect } from "@ngrx/effects";
-import { catchError, map, of, switchMap } from "rxjs";
+import { catchError, map, of, switchMap, tap } from "rxjs";
 import { environment } from "src/environments/environment";
 import * as AuthActions from './auth.actions';
 
@@ -31,15 +32,34 @@ export class AuthEffects {
             ).pipe(
                 map(resData => {
                     const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-                    return of(new AuthActions.Login({email: resData.email, userId: resData.localId, token: resData.idToken, expirationDate: expirationDate}));
+                    return new AuthActions.Login({email: resData.email, userId: resData.localId, token: resData.idToken, expirationDate: expirationDate});
                 }), 
-                catchError(error => {
-                // ...
-                return of();
+                catchError(errorRes => {
+                    let errorMessage = 'An unknown error occurred!';
+                    if(!errorRes.error || !errorRes.error.error) {
+                        return of(new AuthActions.LoginFail(errorMessage));
+                    }
+                    switch (errorRes.error.error.message) {
+                        case 'EMAIL_EXISTS': 
+                            errorMessage = 'This email exists already';
+                            break;
+                        case 'EMAIL_NOT_FOUND': 
+                            errorMessage = 'This email does not exist.';
+                            break;
+                        case 'INVALID_PASSWORD': 
+                            errorMessage = 'This password is not correct.';
+                            break;
+                    }
+                return of(new AuthActions.LoginFail(errorMessage));
             }), );
         }), 
 
     );
 
-    constructor(private actions$: Actions, private http: HttpClient){}
+    @Effect({dispatch: false})
+    authSuccess = this.actions$.pipe(ofType(AuthActions.LOGIN), tap(() => {
+        this.router.navigate(['/']);
+    }));
+
+    constructor(private actions$: Actions, private http: HttpClient, private router: Router){}
 }
