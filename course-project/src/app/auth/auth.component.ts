384271bc5b5bd99a692cba0d9171c58e1f2c2e9d
ljh9 <dkssud2208@naver.com>
2022-09-29 20:10:11 +0900

    Deep Learning of Certification Effects

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -20,6 +20,7 @@ export class AuthComponent implements OnInit, OnDestroy {
     @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
 
     private closeSub: Subscription;
+    private storeSub: Subscription;
 
     constructor(private authService: AuthService, private router: Router, 
         private componentFactoryResolver: ComponentFactoryResolver, 
@@ -27,7 +28,7 @@ export class AuthComponent implements OnInit, OnDestroy {
 
 
     ngOnInit(): void {
-        this.store.select('auth').subscribe(authState => {
+        this.storeSub = this.store.select('auth').subscribe(authState => {
             this.isLoading = authState.loading;
             this.error = authState.authError;
             if (this.error){
@@ -77,12 +78,16 @@ export class AuthComponent implements OnInit, OnDestroy {
     }
 
     onHandleError(){
-        this.error = null;
+        // this.error = null;
+        this.store.dispatch(new AuthActions.ClearError());
     }
 
     ngOnDestroy(): void {
         if (this.closeSub){
             this.closeSub.unsubscribe();
+        }
+        if (this.storeSub){
+            this.storeSub.unsubscribe();
         }
     }
 
--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -27,31 +27,31 @@ export class AuthService{
     constructor(private http: HttpClient, private router: Router, 
         private store: Store<fromApp.AppState>) {}
 
-    singup(email: string, password: string){
-        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey, 
-            {
-                email: email, 
-                password: password, 
-                returnSecureToken: true
-            }
-        ).pipe(catchError(this.handleError), tap(resData => {
-            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
-        }));
-    }
+    // singup(email: string, password: string){
+    //     return this.http.post<AuthResponseData>(
+    //         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey, 
+    //         {
+    //             email: email, 
+    //             password: password, 
+    //             returnSecureToken: true
+    //         }
+    //     ).pipe(catchError(this.handleError), tap(resData => {
+    //         this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    //     }));
+    // }
 
-    login(email: string, password: string){
-        return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, 
-            {
-                email: email, 
-                password: password, 
-                returnSecureToken: true
-            }
-        ).pipe(catchError(this.handleError), tap(resData => {
-            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
-        }));
-    }
+    // login(email: string, password: string){
+    //     return this.http.post<AuthResponseData>(
+    //         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, 
+    //         {
+    //             email: email, 
+    //             password: password, 
+    //             returnSecureToken: true
+    //         }
+    //     ).pipe(catchError(this.handleError), tap(resData => {
+    //         this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    //     }));
+    // }
 
     autoLogin(){
         const userData: {
@@ -78,7 +78,7 @@ export class AuthService{
     logout(){
         // this.user.next(null);
         this.store.dispatch(new AuthActions.Logout());
-        this.router.navigate(['/auth']);
+        // this.router.navigate(['/auth']);
         localStorage.removeItem('userData');
         if ( this.tokenExpirationTimer) {
             clearTimeout(this.tokenExpirationTimer);
