99af939bd984d8e530c5a11b3ce7106b698af23f
ljh9 <dkssud2208@naver.com>
2022-09-29 19:44:58 +0900

    Add Signup

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -47,15 +47,16 @@ export class AuthComponent implements OnInit, OnDestroy {
         const email = form.value.email;
         const password = form.value.password;
 
-        let authObs: Observable<AuthResponseData>;
+        // let authObs: Observable<AuthResponseData>;
 
-        this.isLoading = true;
+        // this.isLoading = true;
 
         if(this.isLoginMode) {
             // authObs = this.authService.login(email, password);
             this.store.dispatch(new AuthActions.LoginStart({email: email, password: password}));
         } else {
-            authObs = this.authService.singup(email, password)
+            // authObs = this.authService.singup(email, password)
+            this.store.dispatch(new AuthActions.SignupStart({email: email, password: password}))
         }
 
         // authObs.subscribe(
