e65993a49c9f56a53c7673f5e056d57fc93b8e80
ljh9 <dkssud2208@naver.com>
2022-09-30 20:34:45 +0900

    Getting Recipe and Using Resolver

--- course-project/src/app/recipes/recipes-resolver.service.ts
+++ course-project/src/app/recipes/recipes-resolver.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs";
+import { Store } from "@ngrx/store";
+import { Observable, take } from "rxjs";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Recipe } from "./recipe.model";
 import { RecipeService } from "./recipe.service";
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+import { Actions, ofType } from "@ngrx/effects";
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]>{
-    constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService){}
+    // constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService){}
+    constructor(private stroe: Store<fromApp.AppState>, private actions$: Actions){}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const recipes = this.recipesService.getRecipes();
+        // const recipes = this.recipesService.getRecipes();
 
-        if(recipes.length ===0 ){
-            return this.dataStorageService.fetchRecipes();
-        }else{
-            return recipes;
-        }
+        // if(recipes.length ===0 ){
+        //     return this.dataStorageService.fetchRecipes();
+        // }else{
+        //     return recipes;
+        // }
+        this.stroe.dispatch(new RecipesActions.FetchRecipes());
+        return this.actions$.pipe(ofType(RecipesActions.SET_RECIPES), take(1))
     }
 }
