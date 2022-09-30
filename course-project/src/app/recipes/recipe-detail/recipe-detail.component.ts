8e115763ba5d936e225e06045aa54bc678e9a964
ljh9 <dkssud2208@naver.com>
2022-09-30 20:16:54 +0900

    Get Recipe Detail Data

--- course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import * as fromApp from '../../store/app.reducer';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,13 +17,26 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(private recipeService: RecipeService, 
     private route: ActivatedRoute, 
-    private router: Router) { }
+    private router: Router, 
+    private store: Store<fromApp.AppState>
+    ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.id = +params['id'];
-        this.recipe = this.recipeService.getRecipe(this.id);
+    // this.route.params.subscribe(
+    this.route.params.pipe(map(params => {
+      return +params['id']
+    }), switchMap(id => {
+      this.id = id;
+      return this.store.select('recipes');
+    }), 
+    map(recipesState => {
+      return recipesState.recipes.find((recipe, index) => {
+        return index === this.id;
+      });
+    }))
+    // this.recipe = this.recipeService.getRecipe(this.id);
+    .subscribe(recipe => {
+      this.recipe =recipe;
       }
     );
   }
