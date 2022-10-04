4dffe238f87469298499f2a57b9eedc61a8db3e8
ljh9 <dkssud2208@naver.com>
2022-10-05 00:42:12 +0900

    Adding components and some fitting tests

--- testing-final/src/app/user/user.component.ts
+++ testing-final/src/app/user/user.component.ts
@@ -6,19 +6,15 @@ import { DataService } from "../shared/data.service";
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  providers: [UserService, DataService]
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
   user: {name: string};
   isLoggedIn = false;
-  data: string;
 
-  constructor(private userService: UserService, private dataService: DataService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.user = this.userService.user;
-    this.dataService.getDetails().then((data: string) => this.data = data);
   }
 
 }
