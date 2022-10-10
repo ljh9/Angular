17eb54db5e1b70ea0708f8eaf65a32c59f6e7a38
ljh9 <dkssud2208@naver.com>
2022-10-11 00:12:19 +0900

    Understanding Generics

--- typescript-intro/basics.ts
+++ typescript-intro/basics.ts
@@ -55,3 +55,18 @@ function addNumbers(a: number, b: number) {
 function printOutput(value: any) {
   console.log(value);
 }
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+// updatedArray[0].split('');
+// stringArray[0].split('');
